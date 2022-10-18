8e157febf4ca8ff9c677e7757b7b3b58fed0f1df
Lukyianenko <110479686+Lukyianenko@users.noreply.github.com>
2022-10-18 11:39:51 +0300

    fix

--- src/slick.js
+++ src/slick.js
@@ -1,6 +1,6 @@
 $('.autoplay').slick({
-    slidesToShow: 3,
-    slidesToScroll: 1,
-    autoplay: true,
-    autoplaySpeed: 2000,
-  });
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  autoplay: true,
+  autoplaySpeed: 2000,
+});
